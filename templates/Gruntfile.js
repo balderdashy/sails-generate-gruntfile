0d63082dcda427e1d26ac93cf35affd378743733
Mike McNeil <michael.r.mcneil@gmail.com>
2014-01-19 23:09:33 -0600

    Formatting, use path.join

--- templates/Gruntfile.js
+++ templates/Gruntfile.js
@@ -14,7 +14,7 @@
  * http://gruntjs.com/configuring-tasks
  */
 
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
 
 
@@ -106,34 +106,36 @@ module.exports = function (grunt) {
   /////////////////////////////////////////////////////////////////
 
   // Modify css file injection paths to use
-  cssFilesToInject = cssFilesToInject.map(function (path) {
+  cssFilesToInject = cssFilesToInject.map(function(path) {
     return '.tmp/public/' + path;
   });
 
   // Modify js file injection paths to use
-  jsFilesToInject = jsFilesToInject.map(function (path) {
+  jsFilesToInject = jsFilesToInject.map(function(path) {
     return '.tmp/public/' + path;
   });
 
 
-  templateFilesToInject = templateFilesToInject.map(function (path) {
+  templateFilesToInject = templateFilesToInject.map(function(path) {
     return 'assets/' + path;
   });
 
 
   // Get path to core grunt dependencies from Sails
+  var path = require('path');
   var depsPath = grunt.option('gdsrc') || 'node_modules/sails/node_modules';
-  grunt.loadTasks(depsPath + '/grunt-contrib-clean/tasks');
-  grunt.loadTasks(depsPath + '/grunt-contrib-copy/tasks');
-  grunt.loadTasks(depsPath + '/grunt-contrib-concat/tasks');
-  grunt.loadTasks(depsPath + '/grunt-sails-linker/tasks');
-  grunt.loadTasks(depsPath + '/grunt-contrib-jst/tasks');
-  grunt.loadTasks(depsPath + '/grunt-contrib-watch/tasks');
-  grunt.loadTasks(depsPath + '/grunt-contrib-uglify/tasks');
-  grunt.loadTasks(depsPath + '/grunt-contrib-cssmin/tasks');
-  grunt.loadTasks(depsPath + '/grunt-contrib-less/tasks');
-  grunt.loadTasks(depsPath + '/grunt-contrib-coffee/tasks');
-  grunt.loadTasks(depsPath + '/grunt-sync/tasks');
+  grunt.loadTasks(path.join(depsPath, '/grunt-contrib-clean/tasks'));
+  grunt.loadTasks(path.join(depsPath, '/grunt-contrib-copy/tasks'));
+  grunt.loadTasks(path.join(depsPath, '/grunt-contrib-concat/tasks'));
+  grunt.loadTasks(path.join(depsPath, '/grunt-sails-linker/tasks'));
+  grunt.loadTasks(path.join(depsPath, '/grunt-contrib-jst/tasks'));
+  grunt.loadTasks(path.join(depsPath, '/grunt-contrib-watch/tasks'));
+  grunt.loadTasks(path.join(depsPath, '/grunt-contrib-uglify/tasks'));
+  grunt.loadTasks(path.join(depsPath, '/grunt-contrib-cssmin/tasks'));
+  grunt.loadTasks(path.join(depsPath, '/grunt-contrib-less/tasks'));
+  grunt.loadTasks(path.join(depsPath, '/grunt-contrib-coffee/tasks'));
+  grunt.loadTasks(path.join(depsPath, '/grunt-sync/tasks'));
+
 
   // Project configuration.
   grunt.initConfig({
@@ -141,24 +143,20 @@ module.exports = function (grunt) {
 
     copy: {
       dev: {
-        files: [
-          {
+        files: [{
           expand: true,
           cwd: './assets',
           src: ['**/*'],
           dest: '.tmp/public'
-        }
-        ]
+        }]
       },
       build: {
-        files: [
-          {
+        files: [{
           expand: true,
           cwd: '.tmp/public',
           src: ['**/*'],
           dest: 'www'
-        }
-        ]
+        }]
       }
     },
 
@@ -195,8 +193,7 @@ module.exports = function (grunt) {
 
     less: {
       dev: {
-        files: [
-          {
+        files: [{
           expand: true,
           cwd: 'assets/styles/',
           src: ['*.less'],
@@ -208,36 +205,32 @@ module.exports = function (grunt) {
           src: ['*.less'],
           dest: '.tmp/public/linker/styles/',
           ext: '.css'
-        }
-        ]
+        }]
       }
     },
 
     coffee: {
       dev: {
-        options:{
-          bare:true,
+        options: {
+          bare: true,
           sourceMap: true,
           sourceRoot: './'
         },
-        files: [
-          {
-            expand: true,
-            cwd: 'assets/js/',
-            src: ['**/*.coffee'],
-            dest: '.tmp/public/js/',
-            ext: '.js'
-          }, {
-            expand: true,
-            cwd: 'assets/linker/js/',
-            src: ['**/*.coffee'],
-            dest: '.tmp/public/linker/js/',
-            ext: '.js'
-          }
-        ]
+        files: [{
+          expand: true,
+          cwd: 'assets/js/',
+          src: ['**/*.coffee'],
+          dest: '.tmp/public/js/',
+          ext: '.js'
+        }, {
+          expand: true,
+          cwd: 'assets/linker/js/',
+          src: ['**/*.coffee'],
+          dest: '.tmp/public/linker/js/',
+          ext: '.js'
+        }]
       }
     },
-
 
 
 
