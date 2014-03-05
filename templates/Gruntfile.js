51f4438f3d35bcb753c12c860ba834d47e7c297b
Mike McNeil <michael.r.mcneil@gmail.com>
2014-03-04 19:46:11 -0600

    'try harder' to get the include-all dep.

--- templates/Gruntfile.js
+++ templates/Gruntfile.js
@@ -46,15 +46,20 @@ module.exports = function(grunt) {
 	var includeAll;
 	try {
 		includeAll = require('include-all');
-	} catch (e) {
-		console.error('Grunt:\nCould not find `include-all` module.');
-		console.error('Skipping grunt tasks...');
-		console.error('To fix this, please run:');
-		console.error('npm install include-all --save`');
-		console.error();
+	} catch (e0) {
+		try {
+			includeAll = require('sails/node-modules/include-all');
+		}
+		catch(e1) {
+			console.error('Grunt:\nCould not find `include-all` module.');
+			console.error('Skipping grunt tasks...');
+			console.error('To fix this, please run:');
+			console.error('npm install include-all --save`');
+			console.error();
 
-		grunt.registerTask('default', []);
-		return;
+			grunt.registerTask('default', []);
+			return;
+		}
 	}
 
 
